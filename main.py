9ce3ae6eec0ed54ebdec64ec476f82a6834fbd90
michaldra <mich.dra10@gmail.com>
2024-08-13 19:31:04 +0200

    hghgh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -9,23 +10,67 @@ db = SQLAlchemy(app)
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    subtitle = db.Column(db.String(100), nullable=False)
+    subtitle = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
+
     def __repr__(self):
         return f'<Card {self.id}>'
-
-@app.route('/')
-def index():
-    cards= Card.query.order_by(Card.id).all()
     
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(32), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
-    return render_template('index.html',
-                           cards = cards
-                           )
+    def __repr__(self):
+        return f'<User {self.id}>'
+
+@app.route('/', methods=['GET','POST'])
+def login():
+        error = ''
+        if request.method == 'POST':
+            form_email = request.form['email']
+            form_password = request.form['password']
+            users_db = User.query.all()
+
+            for user in users_db:
+                print(form_email)
+                print(form_password)
+                print(user.password)
+                if form_email == user.email and form_password == user.password:
+                    return redirect('/index')
+            else:
+                error = 'Incorrect login or password'
+                return render_template('login.html', error=error)
+        else:
+            error = 'Incorrect login or password'
+            return render_template('login.html', error=error)
+        
+
+@app.route('/reg', methods=['GET','POST'])
+def reg():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/')
+    
+    else:    
+        return render_template('registration.html')
+
+
+@app.route('/index')
+def index():
+    cards = Card.query.order_by(Card.id).all()
+    return render_template('index.html', cards=cards)
 
 @app.route('/card/<int:id>')
 def card(id):
     card = Card.query.get(id)
+
     return render_template('card.html', card=card)
 
 @app.route('/create')
@@ -39,13 +84,17 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        new_card = Card(title=title, subtitle=subtitle, text=text)
-        db.session.add(new_card)
-        db.session.commit()
+        card = Card(title=title, subtitle=subtitle, text=text)
 
-        return redirect('/')
+        db.session.add(card)
+        db.session.commit()
+        return redirect('/index')
     else:
         return render_template('create_card.html')
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
